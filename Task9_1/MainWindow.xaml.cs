078125c51b905d41a947ee5d0bc04548906a1323
Kseniya_Sauk <kseniya_sauk@epam.com>
2016-10-12 14:41:05 +0300

    change wpf

--- Task9_1/MainWindow.xaml.cs
+++ Task9_1/MainWindow.xaml.cs
@@ -29,26 +29,84 @@ namespace Task9_1
             InitializeComponent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void button_Click_1(object sender, RoutedEventArgs e)
         {
             FolderBrowserDialog fbd = new FolderBrowserDialog();
             DialogResult result = fbd.ShowDialog();
+            string targetDirectory = fbd.SelectedPath;
 
-            if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            if (!string.IsNullOrWhiteSpace(targetDirectory))
             {
 
-                var files = Directory.GetFiles(fbd.SelectedPath);
-                string str = "";
-                foreach (var file in files)
+                GetDllFiles(targetDirectory);
+                //listBox_SelectionChanged(sender, );
+
+                System.Windows.Forms.MessageBox.Show($@"Path: {targetDirectory}");
+
+            }
+        }
+
+        public void GetDllFiles(string targetDirectory)
+        {
+            var files = Directory.GetFiles(targetDirectory);
+            foreach (var file in files)
+            {
+                if (file.Contains(".dll"))
                 {
-                    str += file + ";";
+                    listBox.Items.Add(file);
                 }
-                //var scrollBarSizeBoxState = System.Windows.Forms.VisualStyles.ScrollBarSizeBoxState.LeftAlign;
-                System.Windows.Forms.MessageBox.Show($"Path: {fbd.SelectedPath}, Files: {str}");
                 
             }
         }
 
-       
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                // Set filter for file extension and default file extension 
+                DefaultExt = ".dll",
+                Filter = "Dll Files (*.dll)|*.dll"
+            };
+            
+            // Display OpenFileDialog by calling ShowDialog method 
+            bool? result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                ChooseDllFile(filename);
+                //listBox_Copy.Items.Add(filename);
+            }
+        }
+
+        public void ChooseDllFile(string filename)
+        {
+            var assembly = Assembly.ReflectionOnlyLoadFrom(filename);
+            var properties = assembly.GetTypes();
+            foreach (var propertie in properties)
+            {
+                listBox_Copy.Items.Add(propertie);
+            }
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        //private bool listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+
+        //    if (!listBox.Items.IsEmpty)
+        //    {
+        //        return true;
+        //    }
+
+        //    return false;
+        //}
     }
 }
